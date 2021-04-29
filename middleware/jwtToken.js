d4127b37e13efa6fbc818ed2bdd36a9ea8854756
zzangsemin <1015tpals@gmail.com>
2021-04-29 11:42:07 +0900

    Feat: token 검증 미들웨어 추가

---
+++ middleware/jwtToken.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: 'Auth error' });
+  }
+
+  const token = authorization.split(' ')[1];
+  const decoded = jwt.verify(token, process.env.accessSecret);
+  if (!decoded) {
+    return res.status(401).send({ message: 'Auth error' });
+  }
+  req.user = decoded;
+  next();
+};
--- routes/zzim.js
+++ routes/zzim.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const zzimsController = require('../controllers');
 const router = express.Router();
+const jwtMiddleware = require('../middleware/jwtToken');
 
-router.get('/', zzimsController.zzims.zzimList);
-router.post('/', zzimsController.zzims.createZzim);
+router.get('/', jwtMiddleware, zzimsController.zzims.zzimList);
+router.post('/', jwtMiddleware, zzimsController.zzims.createZzim);
 
 module.exports = router;
