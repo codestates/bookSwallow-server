01d25dbd70daa6c3db56d5a312aa70e026c1a22e
zzangsemin <1015tpals@gmail.com>
2021-04-29 16:17:22 +0900

    Feat: /comments 댓글작성 API

--- controllers/comments/createComment.js
+++ controllers/comments/createComment.js
@@ -1,3 +1,18 @@
-module.exports = (req, res) => {
-  res.send('comments/createComment');
+const { comment } = require('../../models');
+
+module.exports = async (req, res) => {
+  if (!req.params.id || !req.user.id || !req.body.content) {
+    return res.status(400).end();
+  }
+
+  try {
+    const commentData = await comment.create({
+      user_id: req.user.id,
+      book_id: req.params.id,
+      content: req.body.content,
+    });
+    res.status(201).json(commentData);
+  } catch (e) {
+    res.status(400).end();
+  }
 };
