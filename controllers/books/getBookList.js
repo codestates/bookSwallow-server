93bb94c80b473921a62ac150638cb49dfcabe056
Dodo9128 <wodbs9128@gmail.com>
2021-04-29 16:09:39 +0900

    Feat: Book API

--- controllers/books/getBookList.js
+++ controllers/books/getBookList.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
-  res.send('book/getBookList');
+const { book } = require('../../models');
+
+module.exports = async (req, res) => {
+  // res.send('book/getBookList');
+
+  const bookList = await book.findAll();
+
+  res.status(200).json({ data: bookList });
 };
--- controllers/books/getBookSearch.js
+++ controllers/books/getBookSearch.js
@@ -1,3 +1,27 @@
-module.exports = (req, res) => {
-  res.send('book/getBookSearch');
+const { book } = require('../../models');
+// const sequelize = require('sequelize');
+// const Op = sequelize.Op;
+
+module.exports = async (req, res) => {
+  // res.send('book/getBookSearch');
+
+  // console.log(req.params.id);
+
+  // const bookInfo = await book.findAll({
+  //   where: {
+  //     title: { [Op.like]: '%' + req.params.id + '%' },
+  //   },
+  // });
+
+  const bookInfo = await book.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (!bookInfo) {
+    res.status(404).json({ message: '해당하는 정보를 찾을 수 없습니다' });
+  } else {
+    res.status(200).json({ data: bookInfo, message: '검색에 성공하였습니다' });
+  }
 };
