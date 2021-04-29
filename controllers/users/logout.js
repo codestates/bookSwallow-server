fa849c554c9e8849a416aea2cc4d15acb8563494
Dodo9128 <wodbs9128@gmail.com>
2021-04-29 13:09:54 +0900

    Feat: logout, withdrawal API

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,3 +1,7 @@
 module.exports = (req, res) => {
-  res.send('user/logout');
+  // res.send('user/logout');
+  req.headers.authorization = undefined;
+  // delete req.headers.authorization;
+  console.log(req.headers);
+  res.status(205).json({ message: '로그아웃이 완료되었습니다' });
 };
