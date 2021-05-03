40310a8fd281958557fa41bff8bbc427fe13068e
zzangsemin <1015tpals@gmail.com>
2021-05-03 17:54:16 +0900

    Fix: zzim 리스트 책정보 추가

--- controllers/zzims/zzimList.js
+++ controllers/zzims/zzimList.js
@@ -1,9 +1,24 @@
-const { zzim } = require('../../models');
+const { zzim, book } = require('../../models');
 
 module.exports = async (req, res) => {
   // req.user 에 모든 헤더 정보 존재
 
   const zzimList = await zzim.findAll({
+    include: [
+      {
+        model: book,
+        attributes: [
+          'title',
+          'authors',
+          'url',
+          'description',
+          'publisher',
+          'cover_img',
+          'price',
+          'like_count',
+        ],
+      },
+    ],
     where: { user_id: req.user.id },
   });
 
