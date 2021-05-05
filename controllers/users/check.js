380c05bf3f8e646f66392d18b8e05745df7f5af9
zzangsemin <1015tpals@gmail.com>
2021-05-05 10:59:33 +0900

    Refactor: 응답 데이터 추가

--- controllers/users/check.js
+++ controllers/users/check.js
@@ -1,4 +1,5 @@
 const { user } = require('../../models');
+const { generateAccessToken } = require('../../utils/userFunc');
 
 module.exports = async (req, res) => {
   if (!req.params.id || req.params.id < 0) {
@@ -9,13 +10,34 @@ module.exports = async (req, res) => {
     const data = await user.findOne({
       where: {
         id,
+        del_flag: 'N',
       },
     });
     if (!data) {
       return res.json({ data: null, message: 'no exist' });
     }
     delete data.dataValues.password;
-    res.status(200).json({ data: data, message: 'find data' });
+
+    const payload = {
+      id: data.id,
+      email: data.email,
+      username: data.username,
+    };
+    const accessToken = generateAccessToken(payload);
+
+    res.cookie('authorization', `Bearer ${accessToken}`, {
+      // domain: 'bookswallow.shop',
+      domain: 'localhost',
+      path: '/',
+      maxAge: 24 * 6 * 60 * 10000,
+      sameSite: 'none',
+      httpOnly: true,
+      secure: true,
+    });
+    res.status(200).json({
+      data: { accessToken: accessToken, payload: data },
+      message: 'find data',
+    });
   } catch (e) {
     res.status(400).end();
   }
